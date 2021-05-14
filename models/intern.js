f612101eeccfddaf2d918783ddd7f59d662a9e21
Falence Lemungoh <falencelemungoh@gmail.com>
2021-05-14 10:10:20 +0100

    Done

--- models/intern.js
+++ models/intern.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const validator = require('validator')
 
 const InternSchema = new Schema({
     name: {
@@ -11,7 +12,9 @@ const InternSchema = new Schema({
         type: String,
         required: true,
         maxLength: 100,
-        unique: true
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
     },
     country: {
         type: String, 
